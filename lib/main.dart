27b52d18b67c0ef3cb02140c46230361734c87c2
Faiz Irfanudin <faizirfanudin@gmail.com>
2022-06-27 13:23:06 +0700

    fixing issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:jelajah/presentation/pages/confirmation_page.dart';
-import 'package:jelajah/presentation/pages/main_page.dart';
 import 'package:jelajah/presentation/pages/welcome_page.dart';
 
 void main() => runApp(MyApp());
@@ -13,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(0xFF2F80ED),
       ),
-      home: MainPage(),
+      home: WelcomePage(),
     );
   }
 }
